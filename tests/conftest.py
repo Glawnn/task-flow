7d344b0d67c2141d338b2b7c6f3859cc4ade38a6
Gael <prungael@gmail.com>
2025-03-08 20:54:45 +0100

    test: add tu on Step

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import pytest
 
+
 def pytest_collection_modifyitems(config, items):
     for item in items:
         if "tests/unit/" in item.nodeid:
             item.add_marker(pytest.mark.unit)
         elif "tests/integration/" in item.nodeid:
             item.add_marker(pytest.mark.integration)
-
